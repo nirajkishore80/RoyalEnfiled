83ba80edb53bb67e231ffd612efedec737255ba5
Niraj Kishore <nirajchandankishore80gmail.com>
2020-11-20 22:05:44 +0530

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './src/title/title.component';
+import { ImagesComponent } from './images/images.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    ImagesComponent
   ],
   imports: [
     BrowserModule
